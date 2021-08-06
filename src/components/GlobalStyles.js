845392d444f23cc117a237e9341716d34fc53f61
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-08-06 15:53:33 +0300

    Added global styles file

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    div.container {
+      width: 80%;
+      margin: auto;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    ul {
+      list-style: none;
+    }
+  }
+`;
+
+export default GlobalStyles;
