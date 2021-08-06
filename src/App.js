45786aedc27ee50494be6206058b720d7e443eea
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-08-06 15:59:41 +0300

    Created basic file structure for the Header component and downloaded the logo and background images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import GlobalStyles from "./components/GlobalStyles";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className='App'>my app</div>;
+  return (
+    <div className='App'>
+      <GlobalStyles />
+      <Homepage />
+      my app
+    </div>
+  );
 }
 
 export default App;
