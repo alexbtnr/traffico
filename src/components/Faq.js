eba7c18691ce574fc9de1bfc3d9f2b3b09f1b2b6
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-08-09 21:14:41 +0300

    Added and styled FAQ title, subtitle and image

---
+++ src/components/Faq.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import FaqTitle from "./FaqTitle";
+import SideImage from "./SideImage";
+import confusedMan from "../images/confusedMan.png";
+
+const Faq = () => {
+  return (
+    <StyledFaq className='my-10' id='faq'>
+      <div className='background-color'></div>
+      <div className='container'>
+        <div className='flex-container'>
+          <div className='flex-left'>
+            <FaqTitle />
+          </div>
+          <div className='flex-right'>
+            <SideImage img={confusedMan} imgAlt='Confused Man' />
+          </div>
+        </div>
+      </div>
+    </StyledFaq>
+  );
+};
+
+const StyledFaq = styled.section`
+  position: relative;
+  overflow: hidden;
+
+  .background-color {
+    position: absolute;
+    width: 80%;
+    height: 1500px;
+    top: 0;
+    right: 0;
+    background: #fdede8;
+    z-index: -1;
+    border-radius: 11% 0 0 0;
+  }
+
+  div.container {
+    padding: 10rem 0;
+    div.flex-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 5rem;
+
+      .flex-left {
+        align-self: flex-end;
+        width: 40%;
+      }
+
+      .flex-right {
+        width: 60%;
+        img {
+          width: 120%;
+        }
+      }
+    }
+  }
+`;
+
+export default Faq;
